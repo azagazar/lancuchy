e5db267ac018704abf8f8067f44f3e580511b7e7
AgataZarod <a.zarod@alboalbo.com>
2017-03-30 13:53:29 +0200

    poprawa substr

--- js/lancuchy.js
+++ js/lancuchy.js
@@ -2,11 +2,9 @@ var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na 
 var animal = 'Zielone słonie';
 var animalUpperCased = animal.toUpperCase();
 
-console.log(animalUpperCased);
-
 var textAfter = text.replace('Papugi', animalUpperCased);
 console.log(textAfter.length/2);
 
-var textSubstr = textAfter.substr(0, 50);
+var textSubstr = textAfter.substr(0, 48);
 
 console.log(textSubstr);
